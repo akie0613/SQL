fc37fc4748cf378ff54aab23db9bf57bbe3722c5
akie0613 <69658162+akie0613@users.noreply.github.com>
2020-09-01 15:23:57 +0900

    Add files via upload

--- ドリルA/drillA_level1.sql
+++ ドリルA/drillA_level1.sql
@@ -1,18 +1,18 @@
 --level1-1
 SELECT id,name,kind,balance,date 
-FROM Account
+FROM Account;
 
 --level1-2
 SELECT id 
-FROM Account
+FROM Account;
 
 --level1-3
 SELECT id,balance 
-FROM Account
+FROM Account;
 
 --level1-4
 SELECT * 
-FROM Account
+FROM Account;
 
 --level1-5
 UPDATE Account 
@@ -20,13 +20,13 @@ SET name = 'XXXXX'
 
 --level1-6
 UPDATE Account
-SET balance = 99999999,date = '2018-03-31'
+SET balance = 99999999,date = '2018-03-31';
 
 --level1-7
-INSERT INTO Account VALUES (0642191,'アオキ　ハルカ',1,3640551,'2018-03-13')
-INSERT INTO Account VALUES (1039410,'キノシタ　リュウジ',1,259017,'2017-11-30')
-INSERT INTO Account VALUES (1239855,'タカシナ　ミツル',2,6509773,NULL)
+INSERT INTO Account VALUES (0642191,'アオキ　ハルカ',1,3640551,'2018-03-13');
+INSERT INTO Account VALUES (1039410,'キノシタ　リュウジ',1,259017,'2017-11-30');
+INSERT INTO Account VALUES (1239855,'タカシナ　ミツル',2,6509773,NULL);
 
 --level1-8
-DELETE FROM Account
+DELETE FROM Account;
 
