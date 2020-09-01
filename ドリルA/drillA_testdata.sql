dc1a57b44cb63db7f360eb4517734121be2d30f2
akie0613 <69658162+akie0613@users.noreply.github.com>
2020-09-01 15:08:17 +0900

    level1

---
+++ ドリルA/drillA_testdata.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Account( 
+	id char(7) primary key, 
+	name VARCHAR(40) not null, 
+	kind CHAR(1) NOT NULL , 
+	balance INTEGER , 
+	date date
+);
+
+INSERT INTO Account 
+(id,name,kind,balance,date) 
+VALUES 
+(1234567,'サトウ　イチロウ',1,2000300,'2019-03-19'),
+(8901234,'スズキ　ジロウ',1,345900,'2018-10-10'),
+(5678901,'タカハシ　サブロウ',2,8989898,NULL)
