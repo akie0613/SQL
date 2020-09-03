02d62542694f839539a9b2ac8588419a7ecf4c4b
akie0613 <69658162+akie0613@users.noreply.github.com>
2020-09-03 16:54:44 +0900

    level4_修正

--- ドリルC/drillC_level4.sql
+++ ドリルC/drillC_level4.sql
@@ -1,7 +1,7 @@
 --LEVEL4-33
 SELECT 名称 AS なまえ, HP AS 現在のHP, HP+50 AS 装備後のHP
 FROM パーティー
-WHERE 職業コード IN (11,21);
+WHERE 職業コード IN ('11','21');
 
 --LEVEL4-34
 UPDATE パーティー
@@ -11,17 +11,17 @@ WHERE ID IN ('A01','A03');
 --LEVEL4-35
 SELECT 名称 AS なまえ, HP AS 現在のHP, HP*2 AS 予想されるダメージ
 FROM パーティー
-WHERE 職業コード = 11;
+WHERE 職業コード = '11';
 
 --LEVEL4-36
 SELECT 名称 AS なまえ, CONCAT(HP,'/',MP) AS HPとMP, 
 	CASE 状態コード
-	  WHEN 00 THEN ''
-	  WHEN 01 THEN '眠り'
-	  WHEN 02 THEN '毒'
-	  WHEN 03 THEN '沈黙'
-	  WHEN 04 THEN '混乱'
-	  WHEN 09 THEN '気絶' END AS ステータス
+	  WHEN '00' THEN ''
+	  WHEN '01' THEN '眠り'
+	  WHEN '02' THEN '毒'
+	  WHEN '03' THEN '沈黙'
+	  WHEN '04' THEN '混乱'
+	  WHEN '09' THEN '気絶' END AS ステータス
 FROM パーティー;
 
 --LEVEL4-37
@@ -43,22 +43,23 @@ FROM パーティー;
 --LEVEL4-39
 UPDATE パーティー
 SET 状態コード = '04'
-WHERE MP % 4 = 0;
+WHERE HP % 4 = 0 OR MP % 4 = 0;
 
 --LEVEL4-40
 SELECT FLOOR(777 *0.7) AS 支払った金額;
 
 --LEVEL4-41
 UPDATE パーティー
-SET HP = FLOOR(ROUND(HP*1.3,1)),
-MP = FLOOR(ROUND(MP*1.3,1));
+SET HP = FLOOR(ROUND(HP*1.3,0)),
+MP = FLOOR(ROUND(MP*1.3,0));
 
 --LEVEL4-42
 SELECT 名称 AS なまえ,HP, 
 POWER(HP,0) AS 攻撃1回目,
 POWER(HP,1) AS 攻撃2回目,
 POWER(HP,2) AS 攻撃3回目
-FROM パーティー;
+FROM パーティー
+WHERE 職業コード = '10';
 
 --LEVEL4-43
 SELECT 名称 AS なまえ,HP,状態コード,
